03f9cebd14aabfb51f7ae81424019479a54609f8
Paweł Abramowicz <pawelabrams@gmail.com>
2015-05-30 15:56:26 +0200

    Dodano komentarze do klienta

--- client.py
+++ client.py
@@ -8,21 +8,33 @@ from fractions import gcd
 import Pyro4 as pyro
 
 if __name__ == "__main__":
+    #
+    # Some general config
+    #
     start_time = time.time()
     port_number = int(sys.argv[3]) if len(sys.argv) > 3 else 9500
     machine_count = int(sys.argv[1]) # liczba maszyn
     numbers_count = int(sys.argv[2]) # liczba liczb do sprawdzenia NWD
 
+    #
+    # Choose the numbers randomly
+    #
     numbers = []
     for x in range(0, numbers_count):
-        numbers.append((int(sys.argv[4]) if len(sys.argv) > 4 else 1) * random.randint(1, (int(sys.argv[5]) if len(sys.argv) > 5 else 10 ** 12)))
+        numbers.append((int(sys.argv[4]) if len(sys.argv) > 4 else 1) * random.randint(1, (int(sys.argv[5]) if len(sys.argv) > 5 else 10 ** 12))) # mnożnik(4) * losowy_int_z_przedziału[1, max_losowa(5)]
 
     print(numbers)
 
+    #
+    # Server general config
+    #
     servers = ['PYRO:dg@localhost:']
     block_size = int(ceil(numbers_count/machine_count))
     block_count = int(ceil(numbers_count/block_size))
 
+    #
+    # Distribute the numbers
+    #
     machine_no = 0
     srv_array = []
     for y in range(0, block_count):
@@ -34,15 +46,21 @@ if __name__ == "__main__":
         if machine_no % len(servers) == 0:
             port_number += 1
 
+    #
+    # Start the calculations
+    #
     for element in srv_array:
         element.dgcd()
 
+    #
+    # Gather them all
+    #
     one_last_reduce = []
     for element in srv_array:
         one_last_reduce.append(element.getret())
 
-    # one last reduce:
-
+    #
+    # One last reduce:
+    #
     print("The GCD of those is: %s" % reduce(lambda x, y: gcd(x,y), one_last_reduce))
-
     print("--- %s seconds ---" % (time.time() - start_time))
