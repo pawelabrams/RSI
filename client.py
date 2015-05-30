e62c0456a14f36d0057e19ce48dcace6c5954118
Paweł Abramowicz <pawelabrams@gmail.com>
2015-05-30 15:43:33 +0200

    Dodano: port(3) i mnożnik(4) do listy parametrów

--- client.py
+++ client.py
@@ -9,13 +9,13 @@ import Pyro4 as pyro
 
 if __name__ == "__main__":
     start_time = time.time()
-    port_number = 9500
+    port_number = int(sys.argv[3]) if len(sys.argv) > 3 else 9500
     machine_count = int(sys.argv[1]) # liczba maszyn
     numbers_count = int(sys.argv[2]) # liczba liczb do sprawdzenia NWD
 
     numbers = []
     for x in range(0, numbers_count):
-        numbers.append(26*random.randint(1, 10**12))
+        numbers.append((int(sys.argv[4]) if len(sys.argv) > 4 else 1) * random.randint(1, 10**12))
 
     print(numbers)
 
