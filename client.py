d4e2f4e7ca16c0c4030bc90171e8eb7e393c9f37
Paweł Abramowicz <pawelabrams@gmail.com>
2015-05-30 14:49:52 +0200

    Client motzno

--- client.py
+++ client.py
@@ -7,11 +7,8 @@ import Pyro4 as pyro
 if __name__ == "__main__":
     start_time = time.time()
     port_number = 9601
-    machineNumber = float(sys.argv[1])
-    machineNumber = math.sqrt(machineNumber)
-    array_size = int(sys.argv[2])
-    if machineNumber.is_integer() and array_size % machineNumber == 0:
-        client = ClientClass()
-        client.run(machineNumber, port_number, int(sys.argv[3]), int(sys.argv[4]))
-    else:
-        print('Square root of machines count must be an integer!')
+    machine_count = int(sys.argv[1]) # liczba maszyn
+    numbers_count = int(sys.argv[2]) # liczba liczb do sprawdzenia NWD
+
+	servers = ['PYRO:dg@localhost:9500']
+	block_size = int(numbers_count/machine_count)
