779ef2238a565593e284ea522be5bdce5b76b4b5
Paweł Abramowicz <pawelabrams@gmail.com>
2015-06-15 14:49:17 +0200

    Sry, here u are xD

---
+++ client_wrk.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python2
+# coding=utf-8
+import random
+import sys
+import time
+from math import ceil
+from fractions import gcd
+
+from multiprocessing import Process, Queue
+import Pyro4 as pyro
+
+
+def obsluz(url, qi, qo):
+    dg = pyro.Proxy(url)
+    dg.setls(qi.get())
+    #qi.task_done()
+    dg.dgcd()
+    qo.put(dg.getret())
+
+if __name__ == "__main__":
+    # Some general config
+    pyro.config.SERIALIZER = "pickle"
+    port_number = int(sys.argv[3]) if len(sys.argv) > 3 else 9500
+    machine_count = int(sys.argv[1])  # liczba maszyn
+    numbers_count = int(sys.argv[2])  # liczba liczb do sprawdzenia NWD
+
+    # Choose the numbers randomly
+    numbers = []
+    for x in range(0, numbers_count):
+        numbers.append((int(sys.argv[4]) if len(sys.argv) > 4 else 1) * random.randint(1, (
+        int(sys.argv[5]) if len(sys.argv) > 5 else 10 ** 12)))  # mnożnik(4) * losowy_int_z_przedziału[1, max_losowa(5)]
+
+    # print(numbers)
+
+    # Server general config
+    servers = ['PYRO:dg@localhost:']
+    lensrv = len(servers)
+    block_size = int(ceil(numbers_count / machine_count))
+    block_count = int(ceil(numbers_count / block_size))
+
+    # Distribute the numbers
+    srv_array = []
+    for y in range(block_count):
+        url = servers[y % lensrv] + str(port_number)
+        qi = Queue()
+        qo = Queue()
+        dg = Process(target=obsluz, args=(url, qi, qo))
+        dg.start()
+        srv_array.append([dg, qi, qo])
+        if y % lensrv == 0:
+            port_number += 1
+
+		# START THE CLOCKS
+    start_time = time.time()
+
+    machine_no = 0
+    for s in srv_array:
+        s[1].put(numbers[machine_no * block_size:(machine_no + 1) * block_size])
+        machine_no += 1
+
+    # Start the calculations
+    for element in srv_array:
+        element[0].join()
+
+    # Gather them all
+    one_last_reduce = []
+    for element in srv_array:
+        one_last_reduce.append(element[2].get())
+
+    # One last reduce:
+    print("%s, %s" % (reduce(lambda x, y: gcd(x,y), one_last_reduce), (time.time() - start_time))) # 1st: GCD, 2nd: time of exec
